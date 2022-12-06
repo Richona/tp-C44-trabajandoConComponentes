d439754c7b1195b3ef525df5247e8c02bfa8567b
Richona <105828822+Richona@users.noreply.github.com>
2022-12-05 22:08:28 -0300

    tp completado

--- src/components/ContentWrapper.js
+++ src/components/ContentWrapper.js
@@ -2,6 +2,7 @@ import React from 'react';
 import TopBar from './TopBar';
 import ContentRowTop from './ContentRowTop';
 import Footer from './Footer';
+import ContentBeforeFooter from './ContentBeforeFooter';
 function ContentWrapper(){
     return (
         <React.Fragment>
@@ -11,6 +12,7 @@ function ContentWrapper(){
                 <div id="content">
                     <TopBar />
                     <ContentRowTop />
+                    <ContentBeforeFooter />
                     <Footer />
                 </div>
             </div>    
